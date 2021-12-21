9dec05c3a5d4abab47e0d2776f5939e86d2338dc
Daniil Baunov <daniil@baunov.io>
2021-12-21 19:06:50 +0300

    configure pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GiphyModule } from './giphy/giphy.module';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, GiphyModule.forRoot(environment.giphyApiKey)],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        GiphyModule.forRoot(environment.giphyApiKey),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
