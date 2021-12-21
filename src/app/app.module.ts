de5a6e9ada2744b6af16ec6ec8d9ab8cdc147773
Daniil Baunov <daniil@baunov.io>
2021-12-21 18:56:04 +0300

    remove bootstrap import from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GiphyModule } from './giphy/giphy.module';
 import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, NgbModule, GiphyModule.forRoot(environment.giphyApiKey)],
+    imports: [BrowserModule, AppRoutingModule, GiphyModule.forRoot(environment.giphyApiKey)],
     providers: [],
     bootstrap: [AppComponent],
 })
